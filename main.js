d102c0d178f45c4ec3b165a437159645dc2b48a0
Yousaf <musadali996@gmail.com>
2023-02-09 19:22:14 +0200

    edited8

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+let Lineprog = document.querySelector(".line-basic");
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    Lineprog.style.width = scrolled + "%";
+}
+
+
 // Start Menu
 
 let MenuIcon = document.querySelector(".menu")
@@ -19,12 +29,13 @@ let header = document.querySelector("nav");
 window.onscroll = function() {
     this.scrollY >= 200 ? toTop.classList.add("show") : toTop.classList.remove("show");
 
-    if(window.scrollY >= 100) {
+    if(window.scrollY >= 60) {
         header.style.backgroundColor = "#33363e";
     }
     else {
         header.style.backgroundColor = "transparent";
     }
+    myFunction();
 }
 
 toTop.onclick = function () {
@@ -35,3 +46,30 @@ toTop.onclick = function () {
 };
 
 // End Header
+
+
+// Start JSON
+let Time = document.querySelectorAll(".time")
+// let SubmitBtn = document.querySelector(".submit")
+let options = document.querySelectorAll("#select option")
+let myreq = new XMLHttpRequest();
+
+myreq.onreadystatechange = function() {
+    if(this.readyState === 4&& this.status === 200) {
+        let times = JSON.parse(this.responseText);
+        check(times);
+    }
+}
+myreq.open("GET", "times.json", true);
+myreq.send();
+
+
+function check(info) {
+    options.forEach((e)=> {
+        e.onclick = function() {
+            console.log("options")
+        }
+    })
+}
+
+// End JSON
